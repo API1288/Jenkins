c2233872e410b7b203f4e78c9f530f3d6a5e05cb
API1288 <86962985+API1288@users.noreply.github.com>
2021-07-05 17:31:58 +0530

    third commit

--- simple.java
+++ simple.java
@@ -1,6 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
-	ystem.out.println("Hello git");
+	System.out.println("Hello git");
+	    System.out.println("demo github");
     }  
 }  
